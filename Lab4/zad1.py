0ff71d20198bc09af938146f7ff29038b491a9a6
Wemstar <sylwestermacura@gmail.com>
2015-04-20 20:00:47 +0200

    Ukończone Laboratorium 4

--- Lab4/zad1.py
+++ Lab4/zad1.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Ilość kroków czasowych
 N = 1000
+# Ilośc ścieżek
 M = 100
+# Wybór ruchu
 choice = [-1., 1.]
+# Ruch wzdłuż osi X
 x = np.random.choice(choice, size=[N, M])
+# Ruch wzdłuż osi Y
 y = np.random.choice(choice, size=[N, M])
+
+# Położenie w kolejnych krokach czsowych
 x = np.cumsum(x, axis=0)
 y = np.cumsum(y, axis=0)
+
+# Rysowanie
+fig = plt.figure(0.5)
+fig.add_subplot(121)
 plt.plot(x, y)
+fig.add_subplot(122)
+plt.plot(x[:, -1], y[:, -1])
 plt.savefig('zad1.png')
 plt.show()
 
--- Lab4/zad2.py
+++ Lab4/zad2.py
@@ -3,15 +3,22 @@ __author__ = 'wemstar'
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Ilość kroków czasowych
 N = 1000
+# Ilośc ścierzek
 M = 100
+# Wybór punktów
 choice = [-1., 1.]
 wynik = []
+# główna pętla
 for n in range(N):
+    # wybieramy punkty
     x = np.random.choice(choice, size=[n, M])
+    # Sumujemy pod koniec ruchu
     x = np.sum(x, axis=0)
     wynik.append(np.sqrt(np.mean(x ** 2.0)))
+# plotujemy wynik
 plt.plot(wynik)
 plt.plot(np.sqrt(np.arange(len(wynik))))
-plt.savefig('zad1.png')
+plt.savefig('zad2.png')
 plt.show()
